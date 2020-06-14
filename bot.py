18c632a9ac3958978330f0febc3c394470def0b5
weuz1 <51354026+weuz1@users.noreply.github.com>
2020-06-14 21:07:46 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ class MyClient(discord.Client):
         
 client = commands.Bot(command_prefix= '*')
 Clientdiscord = discord.Client()
+client.remove_command('help')
 
 @client.command()
 @commands.has_permissions(kick_members=True)
@@ -78,6 +79,22 @@ async def clear(ctx, amount=None):
 async def _roll(ctx, number:int):
 	num = random.randint(0,random.randint(0,number))
 	await ctx.send(f'Вам выпало: {num}')
+	
+@client.command
+async def help(ctx):
+	embed=discord.Embed(title="Краткая информация", url="https://vk.com/lifestyle_nw", description="Бот, сделанный вместе с NinW1n")
+	embed.set_author(name="zcript info", url="https://vk.com/weu_z",, icon_url="https://sun9-51.userapi.com/c856032/v856032447/221860/cyThQyAZFdU.jpg")
+	embed.set_thumbnail(url="https://media.discordapp.net/attachments/721319431317356609/721770633456910347/image_5.png")
+	embed.add_field(name="*clear (число)", value="очищает выбранное кол-во сообщений ( нужны права администратора )", inline=True)
+	embed.add_field(name="*ban (упомянание)", value="блокирует пользователя ( нужны права на блокировку пользователей )", inline=True)
+	embed.add_field(name="*kick (упомянание)", value="выгоняет пользователя нужны права на исключение пользователей )", inline=True)
+	embed.add_field(name="*ping", value="Показывает ping бота ( что-то типо скорости соединения )", inline=True)
+	embed.add_field(name="*roll (число)", value="Выбирает число от 0 до выбранного вами", inline=True)
+	embed.add_field(name="*8ball (вопрос)", value="Помогает решить выбранный вопрос", inline=True)
+	embed.add_field(name="*to_nether (1-ая координата) (2-ая координата)", value="Переводит ваши координаты в адские ( Minecraft )", inline=True)
+	embed.add_field(name="*to_over (1-ая координата) (2-ая координата)", value="Переводит ваши координаты в обычный мир ( Minecraft )", inline=True)
+	embed.set_footer(text="made by weuz_")
+await self.bot.say(embed=embed)
 	
 # RUN
 client.run(os.environ['DISCORD_TOKEN'])
