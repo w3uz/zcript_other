d3414331ed451f669987b13cd4de6eebcd70a2f0
weuz1 <51354026+weuz1@users.noreply.github.com>
2020-06-30 19:22:10 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -160,7 +160,7 @@ async def case(ctx):
 @case.error
 async def case_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
-        await ctx.send(f'У вас задержка на эту команду. Повторите через {:.2f /60} секунд'.format(error.retry_after)')
+        await ctx.send(f'У вас задержка на эту команду. Повторите через {:.2f}/60 секунд'.format(error.retry_after)')
     else:
         raise error
 
