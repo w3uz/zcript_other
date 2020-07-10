920ba11ae2d180f8218050d53b56669620237187
weuz1 <51354026+weuz1@users.noreply.github.com>
2020-07-10 13:31:46 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ class MyClient(discord.Client):
 	async def on_ready(self):
 		print('Logged on as {0}!'.format(self.user))
         
-client = commands.Bot(command_prefix= '*')
+client = commands.Bot(command_prefix= 'z!')
 Clientdiscord = discord.Client()
 client.remove_command('help')
 
@@ -89,14 +89,14 @@ async def _info(ctx):
 	embed=discord.Embed(title="Краткая информация", url="https://discord.gg/AKd63AS", description="Бот сделанный почти полностью в одиночку")
 	embed.set_author(name="zcript info", url="https://vk.com/weu_z", icon_url="https://sun9-51.userapi.com/c856032/v856032447/221860/cyThQyAZFdU.jpg")
 	embed.set_thumbnail(url="https://media.discordapp.net/attachments/721319431317356609/721770633456910347/image_5.png")
-	embed.add_field(name="*clear (число)", value="очищает выбранное кол-во сообщений ( нужны права администратора )", inline=True)
-	embed.add_field(name="*ban (упомянание)", value="блокирует пользователя ( нужны права на блокировку пользователей )", inline=True)
-	embed.add_field(name="*kick (упомянание)", value="выгоняет пользователя нужны права на исключение пользователей )", inline=True)
-	embed.add_field(name="*ping", value="Показывает ping бота ( что-то типо скорости соединения )", inline=True)
-	embed.add_field(name="*roll (число)", value="Выбирает число от 0 до выбранного вами числа", inline=True)
-	embed.add_field(name="*8ball (вопрос)", value="Помогает решить выбранный вопрос", inline=True)
-	embed.add_field(name="*dollar", value="Показывает актуальный курс доллара", inline=True)
-	embed.add_field(name="*case", value="Самые обычные кейсы, может выпасть: Ничего, Личная роль, Личный цвет. Кулдаун 24ч", inline=True)
+	embed.add_field(name="clear (число)", value="очищает выбранное кол-во сообщений ( нужны права администратора )", inline=True)
+	embed.add_field(name="ban (упомянание)", value="блокирует пользователя ( нужны права на блокировку пользователей )", inline=True)
+	embed.add_field(name="kick (упомянание)", value="выгоняет пользователя нужны права на исключение пользователей )", inline=True)
+	embed.add_field(name="ping", value="Показывает ping бота ( что-то типо скорости соединения )", inline=True)
+	embed.add_field(name="roll (число)", value="Выбирает число от 0 до выбранного вами числа", inline=True)
+	embed.add_field(name="8ball (вопрос)", value="Помогает решить выбранный вопрос", inline=True)
+	embed.add_field(name="dollar", value="Показывает актуальный курс доллара", inline=True)
+	embed.add_field(name="case", value="Самые обычные кейсы, может выпасть: Ничего, Личная роль, Личный цвет. Кулдаун 24ч", inline=True)
 	embed.set_footer(text="zcript devs: discord.gg/pC8wTaj")
 	await author.send(embed=embed)
 	
